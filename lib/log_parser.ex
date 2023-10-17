dcb4eb30c9e959daac9531ea16a4bbe0f8a1c9cf
pabloofdz <92015741+pabloofdz@users.noreply.github.com>
2023-10-17 20:01:29 +0200

    Create log_parser.ex

---
+++ lib/log_parser.ex
@@ -0,0 +1,20 @@
+defmodule LogParser do
+  def valid_line?(line) do
+    Regex.match?(~r/^\[(DEBUG|INFO|WARNING|ERROR)\]/, line)
+  end
+
+  def split_line(line) do
+    Regex.split(~r/<(~|\*|=|\-)*>/, line)
+  end
+
+  def remove_artifacts(line) do
+    Regex.replace(~r/end\-of\-line\d+/i, line, "")
+  end
+
+  def tag_with_user_name(line) do
+    case Regex.run(~r/User\s+(\S+)/, line) do
+      [_, user] -> "[USER] #{user} #{line}"
+      _ -> line
+    end
+  end
+end
