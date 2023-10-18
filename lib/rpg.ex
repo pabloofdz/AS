8530333096054080a24db511807558f94b5721af
pabloofdz <92015741+pabloofdz@users.noreply.github.com>
2023-10-18 19:12:12 +0200

    Create rpg.ex

---
+++ lib/rpg.ex
@@ -0,0 +1,37 @@
+defmodule RPG do
+  defmodule Character do
+    defstruct health: 100, mana: 0
+  end
+
+  defmodule LoafOfBread do
+    defstruct []
+  end
+
+  defmodule ManaPotion do
+    defstruct strength: 10
+  end
+
+  defmodule Poison do
+    defstruct []
+  end
+
+  defmodule EmptyBottle do
+    defstruct []
+  end
+
+  defprotocol Edible do
+    def eat(item, character)
+  end
+
+  defimpl Edible, for: LoafOfBread do
+    def eat(_, character), do: {nil, %RPG.Character{character | health: character.health + 5}}
+  end
+
+  defimpl Edible, for: ManaPotion do
+    def eat(item, character), do: {%RPG.EmptyBottle{}, %RPG.Character{character | mana: character.mana + item.strength}}
+  end
+  
+  defimpl Edible, for: Poison do
+    def eat(_, character), do: {%RPG.EmptyBottle{}, %RPG.Character{character | health: 0}}
+  end
+end
